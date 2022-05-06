09fea0b9a90d1a131b58f7a37ea733b5f469e4ff
Bridget Akellian <akellianbv@gmail.com>
2022-05-06 18:21:34 -0400

    break

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -5,7 +5,18 @@ import Item from "./Item";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [filteredItems, setFilteredItems] = useState("");
+  
 
+  function onSearchChange(e) {
+    e.preventDefault();
+    setFilteredItems(e.target.value)
+  }
+
+  const searchResults = items.filter((item) => {
+    return item.name.includes(filteredItems)
+  })
+  
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
@@ -16,12 +27,20 @@ function ShoppingList({ items }) {
     return item.category === selectedCategory;
   });
 
+  
+
+
+//left off: how to get itemsToDisplay and searchResults to both work at the same time. 
+//Add new item to list
   return (
     <div className="ShoppingList">
       <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <Filter onCategoryChange={handleCategoryChange} onSearchChange={onSearchChange} />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
+          <Item key={item.id} name={item.name} category={item.category} />
+        ))}
+        {searchResults.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
