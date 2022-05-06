a2ebe93f17ad9a0232818d4db8dcb7d44b49fbc4
Bridget Akellian <akellianbv@gmail.com>
2022-05-06 18:21:22 -0400

    break

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,12 +1,24 @@
 import React from "react";
+import { useState } from "react/cjs/react.production.min";
 import { v4 as uuid } from "uuid";
+import Item from "./Item";
 
 function ItemForm(props) {
+
+  // const [name, setName] = useState("");
+  // const [category, setCategory] = useState("")
+
+  // const handleNameChange = (e) => {
+  //   setName(e.target.value)
+  //   console.log(setName)
+  // };
+
+
   return (
     <form className="NewItem">
       <label>
         Name:
-        <input type="text" name="name" />
+        <input type="text" name="name" {/*value={name} onChange={handleNameChange}*/} />
       </label>
 
       <label>
